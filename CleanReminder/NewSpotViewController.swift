427295da1d97d0aed337db3095aa4bf72b49ad60
Andrius Shiaulis <shiaulis@gmail.com>
2020-01-03 01:28:07 +0200

    Improve new spot screen date picker

--- CleanReminder/NewSpotViewController.swift
+++ CleanReminder/NewSpotViewController.swift
@@ -8,20 +8,32 @@
 
 import UIKit
 
-class NewSpotViewController: UIViewController {
+class NewSpotViewController: UITableViewController {
 
     @IBOutlet weak var spotNameTextField: UITextField!
+    @IBOutlet weak var lastDateDetailLabel: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
 
     // MARK: - Properties
     var spotName: String? { self.spotNameTextField.text }
-    var lastActionDate: Date? { self.datePicker.date }
+    var lastActionDate: Date?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.spotNameTextField.becomeFirstResponder()
-        self.datePicker.maximumDate = Date()
+        let today = Date()
+        self.datePicker.maximumDate = today
+        updateDateDetailLabel(with: today)
     }
 
+    @IBAction func lastDateChanged(_ sender: UIDatePicker) {
+        updateDateDetailLabel(with: sender.date)
+    }
+
+    private func updateDateDetailLabel(with date: Date) {
+        let dateStringRepresentation = DateFormatter.localizedString(from: date, dateStyle: .long, timeStyle: .none)
+        self.lastDateDetailLabel.text = dateStringRepresentation
+
+    }
 }
